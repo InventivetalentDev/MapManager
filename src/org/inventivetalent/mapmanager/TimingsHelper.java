ba443d607993742e37831d562c7649f9797e0022
inventivetalent <inventivetalent@inventivegames.de>
2017-02-01 11:33:55 +0100

    formatting

--- src/org/inventivetalent/mapmanager/TimingsHelper.java
+++ src/org/inventivetalent/mapmanager/TimingsHelper.java
@@ -12,7 +12,6 @@ public class TimingsHelper {
 
 	static ClassResolver classResolver = new ClassResolver();
 
-
 	public static final boolean             PAPER_SPIGOT = Bukkit.getVersion().contains("Paper") || (MapManagerPlugin.instance != null && MapManagerPlugin.instance.getConfig().getBoolean("paperSpigot", false));
 	static final        Map<String, Object> HANDLER_MAP  = new HashMap<>();
 
@@ -22,7 +21,7 @@ public class TimingsHelper {
 		}
 		Object handler = HANDLER_MAP.get(name);
 		try {
-			classResolver.resolveSilent("co.aikar.timings.Timing","org.spigotmc.CustomTimingsHandler").getDeclaredMethod("startTiming").invoke(handler);
+			classResolver.resolveSilent("co.aikar.timings.Timing", "org.spigotmc.CustomTimingsHandler").getDeclaredMethod("startTiming").invoke(handler);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
@@ -32,7 +31,7 @@ public class TimingsHelper {
 		if (HANDLER_MAP.containsKey(name)) {
 			Object handler = HANDLER_MAP.get(name);
 			try {
-				classResolver.resolveSilent("co.aikar.timings.Timing","org.spigotmc.CustomTimingsHandler").getDeclaredMethod("stopTiming").invoke(handler);
+				classResolver.resolveSilent("co.aikar.timings.Timing", "org.spigotmc.CustomTimingsHandler").getDeclaredMethod("stopTiming").invoke(handler);
 			} catch (Exception e) {
 				throw new RuntimeException(e);
 			}
