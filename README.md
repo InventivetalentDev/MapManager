9a0244a057fba3eaa1f8dd08d7ba2dbef31e56c4
inventivetalent <mail@inventivetalent.org>
2020-08-01 19:16:12 +0200

    remove stuff

--- README.md
+++
@@ -1,3 +0,0 @@
-# MapManager
-
-[![Build Status](https://travis-ci.org/InventivetalentDev/MapManager.svg?branch=master)](https://travis-ci.org/InventivetalentDev/MapManager)
--- src/org/inventivetalent/mapmanager/wrapper/MultiWrapper.java
+++
@@ -1,9 +0,0 @@
-package org.inventivetalent.mapmanager.wrapper;
-
-import org.inventivetalent.mapmanager.ArrayImage;
-
-public interface MultiWrapper {
-
-	 ArrayImage[][] getMultiContent();
-
-}
